dd50ae516c033c49fb1d43691709cb957c5b20b6
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:14:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-          binding.pry
       string << "#{index + 1}. #{name}"
     end
   end
