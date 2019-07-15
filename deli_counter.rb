1584d0bb72b3837768298fa02f8cab003624b908
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:12:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 katz_deli = []
 
 def line(katz_deli)
@@ -5,6 +7,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+    binding.pry
     katz_deli.each_with_index do |name, index|
       string << "#{index} #{name}"
     end
