a5b72bd324bd5290bd17f66fd7405bea7d97054d
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:15:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@ require "pry"
 katz_deli = []
 
 def line(katz_deli)
-  string = "The line is currently: "
+  string = "The line is currently:"
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      string << "#{index + 1}. #{name}"
+      string << " #{index + 1}. #{name}"
   end
   puts string  
   end
