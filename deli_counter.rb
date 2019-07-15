5f8cce935c3e8708b79de01feb55c4b8d66cde0a
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:21:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving
-  if katz_deli.lenght == 0
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts katz_deli[0]
