8bf811faf3f4c817a35220b6aaa583279f9e8bb4
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:03:22 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,17 @@
+katz_deli = []
+
+def line
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    puts katz_deli
+  end
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving
+  
+end
