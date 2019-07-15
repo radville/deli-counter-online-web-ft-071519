d76d96abcdf309ac16efca7ba967b05ce143d8c3
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:19:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.length == 0
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving
